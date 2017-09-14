2c235c2af188d7e5ca505b302803985774e861f5
gandazhi <18228438700@163.com>
2017-09-14 21:00:07 +0800

    查询获取该节点信息和递归查询获取该节点和下一子节点的信息 (#4)
    * 新建项目,配置项目结构
    
    * 大概完成了登录借口....
    
    * 完成用户注册，退出登录，验证用户名和邮箱是否被使用的接口
    添加MD5加密的类
    
    * 获取用户登录信息
    
    * 完成获取用户找回密码设置的问题和验证用户找回密码设置的问题答案正确性
    
    * 补上漏掉上传的MD5Util
    
    * 用户在没有登录的情况下，验证通过找回密码问题，重置密码
    
    * 获取用户登录信息，更新用户登录信息
    后台管理员登录
    将所有用户相关的请求方式修改为post
    
    * 修改log存储位置
    修复check_valid接口中，type值传没有的值时，直接验证通过的bug
    
    * 管理员新建分类
    管理员更新分类名
    
    * 修复在同节点名下有相同分类名时，也可以创建和更新相同分类名的bug
    
    * 注册的时候验证手机号，邮箱是否合法
    
    * 注册的时候验证邮箱，手机是否合法
    
    * check_valid.do
    验证手机是否合法，验证邮箱是否合法
    
    * forget_get_question.do
    验证传来的username是否为空
    
    * for_get_checkAnswer.do
    验证传入的username，question，answer是否一致
    
    * 优化了些UserServiceImpl的代码，做了写空验证，增加更新用户的手机号码
    
    * 解决冲突
    
    * 查询该节点下的子节点
    递归查询该节点下子节点的所有分类

--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -1,10 +1,17 @@
 package com.mmall.service;
 
 import com.mmall.common.ServiceResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
 
 public interface ICategoryService {
 
     ServiceResponse addCategory(String categoryName, Integer parentId);
 
     ServiceResponse updateCategoryName(String newCategoryName, Integer categoryId);
+
+    ServiceResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServiceResponse selectCategoryAndChildrenById(Integer categoryId);
 }
